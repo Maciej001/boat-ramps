0a77ee35f0f5c0030a3abf2e036d0a149988c4f2
Maciej001 <m.nowakowskipl@gmail.com>
2019-07-24 10:56:32 +0100

    <Dashboard/> moved from function to class

--- src/dashoboard/Dashboard.js
+++ src/dashoboard/Dashboard.js
@@ -1,40 +1,27 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Filters from "./filters/Filters";
 import { PageContainer, PageTitle, MainArea } from "./components/common";
 import Map from "./map/Map";
 import materialTypes from "./filters/material/materialTypes";
 
-const Dashboard = () => {
-  const rampMaterials = [
-    { label: materialTypes.ALL, count: 9 },
-    { label: materialTypes.BITUMEN, count: 7 },
-    { label: materialTypes.EARTH, count: 2 }
-  ];
-
-  // const allMaterialsCount = rampMaterials.reduce(
-  //   (total, material) => total + parseInt(material.count),
-  //   0
-  // );
-
-  // Check localStore for data
-  const localCacheData = JSON.parse(localStorage.getItem("boat.ramps"));
-
-  // Initial data
-  if (localCacheData) {
-  } else {
+class Dashboard extends Component {
+  componentDidMount() {}
+  render() {
+    const materials = [
+      { label: materialTypes.ALL, count: 9 },
+      { label: materialTypes.BITUMEN, count: 7 },
+      { label: materialTypes.EARTH, count: 2 }
+    ];
+    return (
+      <PageContainer>
+        <PageTitle>Boat Ramps</PageTitle>
+        <MainArea>
+          <Filters materials={materials} />
+          <Map features={[]} />
+        </MainArea>
+      </PageContainer>
+    );
   }
-
-  const features = [];
-  return (
-    <PageContainer>
-      <PageTitle>Boat Ramps</PageTitle>
-
-      <MainArea>
-        <Filters materials={rampMaterials} />
-        <Map features={features} />
-      </MainArea>
-    </PageContainer>
-  );
-};
+}
 
 export default Dashboard;
